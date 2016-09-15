239a8d596f31421e1853adf860b00c73aa900e5d
jmkongas <aaronkongsebastian@gmail.com>
2016-09-15 14:58:49 +0800

    Try1

---
+++ drawbox.js
@@ -0,0 +1,11 @@
+// alert("This is working!");
+
+var c =document.getElementById("myCanvas");
+var ctx = c.getContext('2d');
+// ctx.moveTo(0,0);
+// ctx.lineTo(100,200);
+// ctx.stroke();
+ctx.fillStyle = "rgb(0,0,150)";
+ctx.fillRect(100,100,100,100);
+
+
